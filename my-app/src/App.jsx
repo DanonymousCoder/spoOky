ed33f9663eee8b788a164793d7dd934dec6510d2
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2025-10-31 22:33:59 +0100

    Add footer with progress display and update MyComponent for state management. Got to use useRef and did some DOM manipulation

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { useState } from "react";
+import {useRef} from "react";
 
 function App() {
   const [candies, setCandies] = useState(0);
   const [treats, setTreats] = useState(0);
   const [tricks, setTricks] = useState(0);
-
+  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
@@ -101,11 +102,18 @@ function App() {
         setTreats ={setTreats}
         setTricks={setTricks} />
       </div>
+
+      <footer>
+        <div className="progress-top">
+          <p>Progress</p>
+          <ProgressCount />
+        </div>
+        <div className="progress-bar"></div>
+      </footer>
     </div>
   )
 };
 
-const count = 0;
 
 const SoundButton = () => (
   <button>Sound On</button>
@@ -127,27 +135,37 @@ const HighScore = () => {
   return <p className="count">0</p>
 }
 
-const MyComponent = ({candies, setCandies, setTreats, setTricks}) => {
+const ProgressCount = ({count}) => {
+  return <p><span>{count}</span>/12 Doors</p>
+}
+
+const MyComponent = ({count, candies, setCandies, setTreats, setTricks, setCount}) => {
  
   const [isClicked, setIsClicked] = useState("Knock");
+  const getTarget = useRef(null);
 
   const handleClick = () => {
     setIsClicked("Knocked");
-    setCount(count + 1);
+    setCandies(candies + 1);
+    setCount(count + 1)
+    
+    if(getTarget.current) {
+      getTarget.current.classList.add("checked");
+    }
 
    const determiner = Math.floor(Math.random() * 2);
 
    if (determiner == 1) {
-    setTreats(treats + 1);
+    setTreats(prev => prev + 1);
     alert("Have some candy bars");
    } else {
-    setTricks(tricks + 1);
+    setTricks(prev => prev + 1);
     alert("Have some tricks");
    }
   }
 
   return (
-    <div className="card" onClick={handleClick}>
+    <div className="card" onClick={handleClick} ref={getTarget}>
           <p>{isClicked}</p>
     </div>
   );
