091b4af9a1e32ae94be25e61ba34d192c855e308
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2025-10-31 20:22:41 +0100

    Add state management for candies, treats, and tricks; implement MyComponent for interactive cards

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [candies, setCandies] = useState(0);
+  const [treats, setTreats] = useState(0);
+  const [tricks, setTricks] = useState(0);
+
+
   return (
     <div className="container">
       <nav>
@@ -17,43 +23,134 @@ function App() {
       <div className="boxes">
         <div className="box box-1">
           <p>Candies</p>
-          <CandiesCount />
+          <CandiesCount candies = {candies} />
         </div>
         <div className="box box-2">
           <p>Treats found</p>
-          <TreatsFound />
+          <TreatsFound treats = {treats} />
         </div>
         <div className="box box-3">
           <p>Tricks Hit</p>
-          <TricksHit />
+          <TricksHit tricks = {tricks} />
         </div>
         <div className="box box-3">
           <p>Highscore</p>
           <HighScore />
         </div>
       </div>
+
+      <div className="cards">
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+        <MyComponent 
+        candies ={candies}
+        setCandies ={setCandies}
+        setTreats ={setTreats}
+        setTricks={setTricks} />
+      </div>
     </div>
   )
 };
+
+const count = 0;
 
 const SoundButton = () => (
   <button>Sound On</button>
 );
 
-const CandiesCount = () => {
-  return <p className="count">0</p>
+const CandiesCount = ({candies}) => {
+  return <p className="count">{candies}</p>
 };
 
-const TreatsFound = () => {
-  return <p className="count">0</p>
+const TreatsFound = ({treats}) => {
+  return <p className="count">{treats}</p>
 };
 
-const TricksHit = () => {
-  return <p className="count">0</p>
+const TricksHit = ({tricks}) => {
+  return <p className="count">{tricks}</p>
 };
 
 const HighScore = () => {
   return <p className="count">0</p>
 }
+
+const MyComponent = ({candies, setCandies, setTreats, setTricks}) => {
+ 
+  const [isClicked, setIsClicked] = useState("Knock");
+
+  const handleClick = () => {
+    setIsClicked("Knocked");
+    setCount(count + 1);
+
+   const determiner = Math.floor(Math.random() * 2);
+
+   if (determiner == 1) {
+    setTreats(treats + 1);
+    alert("Have some candy bars");
+   } else {
+    setTricks(tricks + 1);
+    alert("Have some tricks");
+   }
+  }
+
+  return (
+    <div className="card" onClick={handleClick}>
+          <p>{isClicked}</p>
+    </div>
+  );
+};
 
 export default App;
